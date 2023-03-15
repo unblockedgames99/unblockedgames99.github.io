75b84001b4b91f8b368f220583b91bcb3e1e881a
SKMGames <41476960+HuySKM@users.noreply.github.com>
2023-03-15 09:06:04 +0700

    index

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,273 +1,310 @@
-$(function ($) {
-    "use strict";
-
-    jQuery(document).ready(function () {
-
-
-        //   magnific popup activation
-        $('.play-video').magnificPopup({
-            type: 'video'
-        });
-
-        $('.img-popup').magnificPopup({
-            type: 'image'
-        });
-
-        // $('#faqModal').modal('show');
-
-        // Product deal countdown
-        $('[data-countdown]').each(function () {
-            var $this = $(this),
-                finalDate = $(this).data('countdown');
-            $this.countdown(finalDate, function (event) {
-                $this.html(event.strftime('<span>%D:</span><span>%H:</span><span>%M:</span><span>%S</span>'));
-            });
-        });
-        $('[data-countdown2]').each(function () {
-            var $this = $(this),
-                finalDate = $(this).data('countdown2');
-            $this.countdown(finalDate, function (event) {
-                $this.html(event.strftime('<p>%D : <span>Days</span></p> <p>%H : <span>Hours</span></p> <p>%M : <span>Minutes</span></p> <p>%S <span>Seconds</span></p>'));
-            });
-        });
+/************* Main Js File ************************
+    Template Name: Faf
+    Author: Themescare
+    Version: 1.0
+    Copyright 2020
+*************************************************************/
 
 
-        // Latest Winner Slider
-        var $l_winner_slider = $('.l-winner-slider2');
-        $l_winner_slider.owlCarousel({
-            loop: true,
-            navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
-            nav: true,
-            dots: false,
-            autoplay: false,
-            margin: 30,
-            autoplayTimeout: 6000,
-            smartSpeed: 1000,
-            responsive: {
-                0: {
-                    items: 2
-                },
-                415: {
-                    items: 4
-                },
-                576: {
-                    items: 4
-                },
-                768: {
-                    items: 5
-                },
-                992: {
-                    items: 7
-                },
-                1200: {
-                    items: 9
-                },
-                1920: {
-                    items: 9
-                }
-            }
-        });
+/*------------------------------------------------------------------------------------
+    
+JS INDEX
+=============
+01 - Menu Toggle JS
+02 - Search Toggle
+03 - Main Slider JS
+04 - Testimonial Slider JS
+05 - Counter JS
+06 - Mouse Cursor JS
+07 - Youtube Popup JS
+08 - Quantity Js
+09 - Range sliders
+10 - Header Scroll
 
-        // Latest Winner Slider
-        var $l_winner_slider2 = $('.l-winner-slider');
-        $l_winner_slider2.owlCarousel({
-            loop: true,
-            navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
-            nav: true,
-            dots: false,
-            autoplay: false,
-            margin: 30,
-            autoplayTimeout: 6000,
-            smartSpeed: 1000,
-            responsive: {
-                0: {
-                    items: 3
-                },
-                576: {
-                    items: 4
-                },
-                768: {
-                    items: 4
-                },
-                992: {
-                    items: 3
-                },
-                1200: {
-                    items: 3
-                },
-                1920: {
-                    items: 3
-                }
-            }
-        });
-
-        // Exclusive Lottery Slider
-        var $ex_lottery_slider = $('.ex-lottery-slider');
-        $ex_lottery_slider.owlCarousel({
-            loop: true,
-            navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
-            nav: true,
-            dots: false,
-            autoplay: false,
-            margin: 30,
-            autoplayTimeout: 6000,
-            smartSpeed: 1000,
-            responsive: {
-                0: {
-                    items: 1
-                },
-                500: {
-                    items: 1
-                },
-                768: {
-                    items: 2
-                },
-                992: {
-                    items: 3
-                },
-                1200: {
-                    items: 3
-                },
-                1920: {
-                    items: 3
-                }
-            }
-        });
-
-        /** Product Details  carousel **/
-        var $product_slider = $('.all-slider');
-        $product_slider.owlCarousel({
-            loop: false,
-            dots: false,
-            nav: true,
-            navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
-            margin: 0,
-            autoplay: false,
-            items: 4,
-            margin: 30,
-            autoplayTimeout: 6000,
-            smartSpeed: 1000,
-            responsive: {
-                0: {
-                    items: 3
-                },
-                768: {
-                    items: 4
-                }
-            }
-        });
-
-        // payment Slider
-        var $method_slider = $('.method-slider');
-        $method_slider.owlCarousel({
-            loop: true,
-            navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
-            nav: true,
-            dots: false,
-            autoplay: false,
-            margin: 0,
-            autoplayTimeout: 6000,
-            smartSpeed: 1000,
-            responsive: {
-                0: {
-                    items: 2
-                },
-                500: {
-                    items: 3
-                },
-                768: {
-                    items: 5
-                },
-                992: {
-                    items: 6
-                },
-                1200: {
-                    items: 7
-                },
-                1920: {
-                    items: 7
-                }
-            }
-        });
-
-        // testimonial-slider
-        var $testimonial_slider = $('.testimonial-slider');
-        $testimonial_slider.owlCarousel({
-            loop: true,
-            navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
-            nav: true,
-            dots: false,
-            autoplay: false,
-            margin: 30,
-            autoplayTimeout: 6000,
-            smartSpeed: 1000,
-            responsive: {
-                0: {
-                    items: 1
-                },
-                768: {
-                    items: 1
-                },
-                960: {
-                    items: 1
-                },
-                1200: {
-                    items: 1
-                }
-            }
-        });
+-------------------------------------------------------------------------------------*/
 
 
-    });
+(function ($) {
+	"use strict";
+
+	jQuery(document).ready(function ($) {
 
 
-    /*-------------------------------
-        back to top
-    ------------------------------*/
-    $(document).on('click', '.bottomtotop', function () {
-        $("html,body").animate({
-            scrollTop: 0
-        }, 2000);
-    });
+		/* 
+		=================================================================
+		01 - Menu Toggle JS
+		=================================================================	
+		*/
+		$(".menu-toggle").on("click", function () {
+			$(this).toggleClass("is-active");
+		});
 
-    //define variable for store last scrolltop
-    var lastScrollTop = '';
-    $(window).on('scroll', function () {
-        var $window = $(window);
-        if ($window.scrollTop() > 0) {
-            $(".header").addClass('nav-fixed');
-        } else {
-            $(".header").removeClass('nav-fixed');
-        }
+		/* 
+		=================================================================
+		02 - Search Toggle
+		=================================================================	
+		*/
+        
+        $("#search-trigger").on("click", function () {
+			$(".search-block").toggleClass("active");
+		});
+        
+        $(".search-block input").on("click", function () {
+			$(".search-block span").toggleClass("active");
+		});
+        
+        $(".search-toggle").on("click", function () {
+			$(".search-block").removeClass("active");
+			$(".search-block span").removeClass("active");
+		});
+        
 
-        /*---------------------------
-            back to top show / hide
-        ---------------------------*/
-        var st = $(this).scrollTop();
-        var ScrollTop = $('.bottomtotop');
-        if ($(window).scrollTop() > 1000) {
-            ScrollTop.fadeIn(1000);
-        } else {
-            ScrollTop.fadeOut(1000);
-        }
-        lastScrollTop = st;
 
-    });
+		/* 
+		=================================================================
+		03 - Main Slider JS
+		=================================================================	
+		*/
 
-    $(window).on('load', function () {
+		$(".fag-slide").owlCarousel({
+			animateOut: 'fadeOutLeft',
+			animateIn: 'fadeIn',
+			items: 2,
+			nav: false,
+			dots: true,
+			autoplayTimeout: 7000,
+			autoplaySpeed: 2000,
+			autoplay: false,
+			loop: true,
+			navText: ["<i class='fa fa-long-arrow-left'><i>", "<i class='fa fa-long-arrow-right'><i>"],
+			mouseDrag: true,
+			touchDrag: true,
+			responsive: {
+				0: {
+					items: 1
+				},
+				480: {
+					items: 1
+				},
+				600: {
+					items: 1
+				},
+				750: {
+					items: 1
+				},
+				1000: {
+					items: 1
+				},
+				1200: {
+					items: 1
+				}
+			}
+		});
 
-        /*---------------------
-            Preloader
-        -----------------------*/
-        var preLoder = $("#preloader");
-        preLoder.addClass('hide');
-        var backtoTop = $('.back-to-top');
-        /*-----------------------------
-            back to top
-        -----------------------------*/
-        var backtoTop = $('.bottomtotop');
-        backtoTop.fadeOut(100);
+		$(".fag-slide").on("translate.owl.carousel", function () {
+			$(".fag-main-slide h2, .fag-main-slide p").removeClass("animated fadeInUp").css("opacity", "0");
+			$(".fag-main-slide .fag-btn-outline").removeClass("animated fadeInDown").css("opacity", "0");
+			$(".fag-main-slide h3").removeClass("animated fadeInLeft").css("opacity", "0");
+		});
+		$(".fag-slide").on("translated.owl.carousel", function () {
+			$(".fag-main-slide h2, .fag-main-slide p").addClass("animated fadeInUp").css("opacity", "1");
+			$(".fag-main-slide .fag-btn-outline").addClass("animated fadeInDown").css("opacity", "1");
+			$(".fag-main-slide h3").addClass("animated fadeInLeft").css("opacity", "1");
+		});
 
-    });
 
-});
+		/* 
+		=================================================================
+		04 - Testimonial Slider JS
+		=================================================================	
+		*/
+
+		$(".testimonial-slider").owlCarousel({
+			margin: 20,
+			nav: true,
+			dots: false,
+			autoplayTimeout: 7000,
+			autoplaySpeed: 2000,
+			autoplay: true,
+			loop: true,
+			navText: ["<i class='fa fa-angle-left'><i>", "<i class='fa fa-angle-right'><i>"],
+			mouseDrag: true,
+			touchDrag: true,
+			responsive: {
+				0: {
+					items: 1
+				},
+				480: {
+					items: 1
+				},
+				600: {
+					items: 1
+				},
+				750: {
+					items: 2
+				},
+				1000: {
+					items: 2
+				},
+				1200: {
+					items: 2
+				}
+			}
+		});
+
+
+
+		/* 
+		=================================================================
+		06 - Mouse Cursor JS
+		=================================================================	
+		*/
+
+
+		$('body').on("mouseup", function () {
+			$('#cursor-small').removeClass("click");
+		});
+
+		$('body').on('click', function (e) {
+			$('#cursor-small').css({
+				left: e.pageX,
+				top: e.pageY
+			});
+			$('#cursor-small').addClass("click");
+		});
+
+		var cursorLarge = $('#cursor-large');
+		var cursorSmall = $('#cursor-small');
+
+		$(document).mousemove(function (e) {
+			var x = e.clientX;
+			var y = e.clientY;
+
+			cursorLarge.css({
+				"left": `${x}px`,
+				"top": `${y}px`
+			});
+			cursorSmall.css({
+				"left": `${x}px`,
+				"top": `${y}px`
+			});
+		});
+
+		/* 
+		=================================================================
+		07 - Youtube Popup JS
+		=================================================================	
+		*/
+		var $animation_elements = $('.heading_animation');
+		var $window = $(window);
+
+		function check_if_in_view() {
+			var window_height = $window.height();
+			var window_top_position = $window.scrollTop();
+			var window_bottom_position = (window_top_position + window_height);
+
+			$.each($animation_elements, function () {
+				var $element = $(this);
+				var element_height = $element.outerHeight();
+				var element_top_position = $element.offset().top + 150;
+				var element_bottom_position = (element_top_position + element_height);
+
+				//check to see if this current container is within viewport
+				if ((element_bottom_position >= window_top_position) &&
+					(element_top_position <= window_bottom_position)) {
+					$element.addClass('extend');
+				}
+			});
+		}
+		$window.on('scroll resize', check_if_in_view);
+		$window.trigger('scroll');
+
+
+		/* 
+		=================================================================
+		07 - Youtube Popup JS
+		=================================================================	
+		*/
+
+		$('.popup-youtube, .play-video').magnificPopup({
+			type: 'iframe',
+			mainClass: 'mfp-fade',
+			preloader: true,
+		});
+
+		/*==============================
+		08 - Quantity Js
+		==============================*/
+        $(".num-in span").on("click", function () {
+			var $input = $(this).parents('.num-block').find('input.in-num');
+			if ($(this).hasClass('minus')) {
+				var count = parseFloat($input.val()) - 1;
+				count = count < 1 ? 1 : count;
+				if (count < 2) {
+					$(this).addClass('dis');
+				} else {
+					$(this).removeClass('dis');
+				}
+				$input.val(count);
+			} else {
+				var count = parseFloat($input.val()) + 1
+				$input.val(count);
+				if (count > 1) {
+					$(this).parents('.num-block').find(('.minus')).removeClass('dis');
+				}
+			}
+
+			$input.change();
+			return false;
+		});
+        
+
+
+		/*==============================
+		09 - Range sliders
+		==============================*/
+
+		if ($('#filter_range').length) {
+			var firstSlider = document.getElementById('filter_range');
+			noUiSlider.create(firstSlider, {
+				range: {
+					'min': 0,
+					'max': 500
+				},
+				step: 1,
+				connect: true,
+				start: [18, 300],
+				format: wNumb({
+					decimals: 0,
+					prefix: '$'
+				})
+			});
+			var firstValues = [
+				document.getElementById('filter_range-start'),
+				document.getElementById('filter_range-end')
+			];
+			firstSlider.noUiSlider.on('update', function (values, handle) {
+				firstValues[handle].innerHTML = values[handle];
+			});
+		} else {
+			return false;
+		}
+
+
+	});
+
+	/*==============================
+	10 - Header Scroll
+	==============================*/
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 50) {
+			$('.fag-header').addClass('scroll');
+		} else {
+			$('.fag-header').removeClass('scroll');
+		}
+
+	});
+
+
+}(jQuery));
+
